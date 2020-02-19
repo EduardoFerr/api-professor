6ba1cf76267fdd09414b426787df5eab2c6ae1f8
Eduardo de Moraes Ferreira <ferr.tutorial@gmail.com>
2020-02-19 11:58:03 -0300

    Procfile

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express()
 var cors = require('cors')
 const porta = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(cors());
+
 app.use('/professores', professor);
 
 app.listen(porta, function () {
